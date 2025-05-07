3d8ab96710c1143be5189949ed426051a30218b5
Gangani <ganganidewmini11@gmail.com>
2025-05-07 23:12:09 +0530

    add

--- backend/src/main/java/backend/BackendApplication.java
+++ backend/src/main/java/backend/BackendApplication.java
@@ -10,7 +10,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.JavaMailSenderImpl;
 
-import java.util.Properties;
+import java.util.Properties; 12
 import org.springframework.context.annotation.Configuration;
 @SpringBootApplication(
 		exclude = {
