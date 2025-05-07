2197c9210ddaff3e32891cfac51705ed8c5a548e
Rashi811 <rashiniharasara@gmail.com>
2025-05-07 19:07:01 +0530

    Latest backend structure

--- backend/src/main/java/backend/PostManagement/repository/PostManagementRepository.java
+++ backend/src/main/java/backend/PostManagement/repository/PostManagementRepository.java
@@ -4,5 +4,5 @@ import backend.PostManagement.model.PostManagementModel;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
 public interface PostManagementRepository extends MongoRepository<PostManagementModel, String> {
-    void deleteByUserID(String userID); // Ensure this method exists
+    void deleteByUserID(String userID);   // Ensure this method exists
 }
