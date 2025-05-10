b2d19d53244458940e941fc5a1ad286393331c0e
Rashi811 <rashiniharasara@gmail.com>
2025-05-10 21:45:34 +0530

    final frontend structure

--- frontend/src/Pages/PostManagement/MyAllPost.js
+++ frontend/src/Pages/PostManagement/MyAllPost.js
@@ -91,7 +91,7 @@ function MyAllPost() {
   const handleDelete = async (postId) => {
     const confirmDelete = window.confirm('Are you sure you want to delete this post?');
     if (!confirmDelete) {
-      return; // Exit if the user cancels the confirmation
+      return;  // Exit if the user cancels the confirmation
     }
 
     try {
